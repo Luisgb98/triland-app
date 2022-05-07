51e500fb44be4417ffad18f83516ce728b9c266b
luisgb98 <luis.guisadobarrera@gmail.com>
2022-05-07 16:20:30 +0200

    Trainings added

--- app/src/main/java/com/luisgb/triland/TrainingActivity.kt
+++ app/src/main/java/com/luisgb/triland/TrainingActivity.kt
@@ -2,10 +2,16 @@ package com.luisgb.triland
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.davemorrissey.labs.subscaleview.ImageSource
+import kotlinx.android.synthetic.main.activity_training.*
 
 class TrainingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_training)
+
+        trainingIV1.setImage(ImageSource.resource(R.drawable.training1))
+        trainingIV2.setImage(ImageSource.resource(R.drawable.training2))
+
     }
 }
