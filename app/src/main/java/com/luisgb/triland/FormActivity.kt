3898d695910ed83448c47a5b9ac61826d1831e56
luisgb98 <luis.guisadobarrera@gmail.com>
2022-05-07 10:31:18 +0200

    Form added

--- app/src/main/java/com/luisgb/triland/FormActivity.kt
+++ app/src/main/java/com/luisgb/triland/FormActivity.kt
@@ -2,10 +2,28 @@ package com.luisgb.triland
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_form.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class FormActivity : AppCompatActivity() {
+
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
+
+        send();
+    }
+
+    private fun send() {
+        sendButton.setOnClickListener{
+            db.collection("form").document(emailET.text.toString()).set(
+                hashMapOf("email" to emailET.text.toString(),
+                    "name" to nameET.text.toString(),
+                    "message" to messageET.text.toString())
+            )
+        }
     }
 }
